e3b4f777c3be3fbc07a072b70b32cc92b6156c4f
Shivansh Kuchchal <shivanshbox@gmail.com>
2022-07-12 23:13:03 +0530

    feat(core): add bridge-database dep

--- src/impl/bridges/database.go
+++ src/impl/bridges/database.go
@@ -7,6 +7,7 @@ import (
 	"github.com/shivanshkc/rosenbridge/src/core/models"
 	"github.com/shivanshkc/rosenbridge/src/mongodb"
 
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
@@ -15,23 +16,71 @@ type Database struct{}
 
 // NewDatabase is a constructor for *Database.
 func NewDatabase() *Database {
-	return nil
+	return &Database{}
 }
 
 func (d *Database) InsertBridge(ctx context.Context, doc *models.BridgeDoc) error {
-	panic("implement me")
+	callCtx, cancelFunc := mongodb.GetTimeoutContext(ctx)
+	defer cancelFunc()
+
+	// Database call.
+	if _, err := mongodb.GetBridgesColl().InsertOne(callCtx, doc); err != nil {
+		return fmt.Errorf("error in GetBridgesColl().InsertOne call: %w", err)
+	}
+
+	return nil
 }
 
 func (d *Database) GetBridgesForClients(ctx context.Context, clientIDs []string) ([]*models.BridgeDoc, error) {
-	panic("implement me")
+	callCtx, cancelFunc := mongodb.GetTimeoutContext(ctx)
+	defer cancelFunc()
+
+	// Creating the required filter.
+	filter := bson.M{"client_id": bson.M{"$in": clientIDs}}
+
+	// Database call.
+	cursor, err := mongodb.GetBridgesColl().Find(callCtx, filter)
+	if err != nil {
+		return nil, fmt.Errorf("error in GetBridgesColl().Find call: %w", err)
+	}
+
+	// Getting documents from the cursor into the slice.
+	var docs []*models.BridgeDoc
+	if err := cursor.All(ctx, &docs); err != nil {
+		return nil, fmt.Errorf("error in cursor.All call: %w", err)
+	}
+
+	return docs, nil
 }
 
 func (d *Database) DeleteBridgeForNode(ctx context.Context, bridgeID string, nodeAddr string) error {
-	panic("implement me")
+	callCtx, cancelFunc := mongodb.GetTimeoutContext(ctx)
+	defer cancelFunc()
+
+	// Creating the required filter.
+	filter := bson.M{"bridge_id": bridgeID, "node_addr": nodeAddr}
+
+	// Database call.
+	if _, err := mongodb.GetBridgesColl().DeleteOne(callCtx, filter); err != nil {
+		return fmt.Errorf("error in GetBridgesColl().DeleteOne call: %w", err)
+	}
+
+	return nil
 }
 
 func (d *Database) DeleteBridgesForNode(ctx context.Context, bridgeIDs []string, nodeAddr string) error {
-	panic("implement me")
+	callCtx, cancelFunc := mongodb.GetTimeoutContext(ctx)
+	defer cancelFunc()
+
+	// Creating the required filter. Note that these conditions use the '&&' operator.
+	filter := bson.M{"node_addr": nodeAddr, "bridge_id": bson.M{"$in": bridgeIDs}}
+
+	// Database call.
+	if _, err := mongodb.GetBridgesColl().DeleteMany(callCtx, filter); err != nil {
+		return fmt.Errorf("error in GetBridgesColl().DeleteMany call: %w", err)
+	}
+
+	return nil
 }
 
 // CreateIndexes creates indexes as per the provided data on the "bridges" collection/table.
