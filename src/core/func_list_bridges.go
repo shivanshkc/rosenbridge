6812294375e42d5deb0d0700d9d93a6fe39f884f
Shivansh Kuchchal <shivanshbox@gmail.com>
2022-07-23 23:27:50 +0530

    feat(core): add list bridges function

--- src/core/func_list_bridges.go
+++ src/core/func_list_bridges.go
@@ -2,11 +2,22 @@ package core
 
 import (
 	"context"
+	"fmt"
 
+	"github.com/shivanshkc/rosenbridge/src/core/deps"
 	"github.com/shivanshkc/rosenbridge/src/core/models"
 )
 
 // ListBridges lists all bridges for the provided clients.
 func ListBridges(ctx context.Context, clientIDs []string) ([]*models.BridgeDoc, error) {
-	panic("implement me")
+	// Getting the dependencies.
+	bridgeDB := deps.DepManager.GetBridgeDatabase()
+
+	// Querying the bridges.
+	bridges, err := bridgeDB.GetBridgesForClients(ctx, clientIDs)
+	if err != nil {
+		return nil, fmt.Errorf("error in bridgeDB.GetBridgesForClients call: %w", err)
+	}
+
+	return bridges, nil
 }
